7163a3c4d556e6ca06ca905f9d8be465dd605fe5
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2023-10-09 11:27:21 -0500

    Changed version to 1.2.0

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -47,7 +47,7 @@ export async function execute(client: Client, interaction: CommandInteraction) {
         },
         {
           name: "Version",
-          value: "1.1.1",
+          value: "1.2.0",
           inline: true,
         },
         {
