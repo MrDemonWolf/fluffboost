8172f7c4c7c2401ba668de92cc27da39ff145991
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2025-07-17 01:40:01 -0500

    Fetches activity from database
    Fetches a random activity from the database to set the bot's status.
    
    If no activities are found in the database, it falls back to using the
    default activity set in the environment variables.

---
+++ src/worker/jobs/setActivity.ts
@@ -0,0 +1,47 @@
+import { ActivityType } from "discord.js";
+import type { Client } from "discord.js";
+import { env } from "../../utils/env";
+import { prisma } from "../../database";
+
+import consola from "consola";
+
+export default async (client: Client) => {
+  try {
+    const defaultActivity = env.DISCORD_DEFAULT_STATUS;
+    const defaultActivityType = env.DISCORD_DEFAULT_ACTIVITY_TYPE;
+    const defaultActivityUrl = env.DEFAULT_ACTIVITY_URL;
+
+    const randomActivity = async () => {
+      const activities = await prisma.discordActivity.findMany();
+
+      if (activities.length === 0) {
+        return null;
+      }
+      const randomIndex = Math.floor(Math.random() * activities.length);
+      return activities[randomIndex];
+    };
+    const activity = await randomActivity();
+    if (!activity) {
+      consola.warn("No activity found, using default activity.");
+      client.user?.setActivity(defaultActivity, {
+        type: ActivityType[defaultActivityType],
+        url: defaultActivityUrl,
+      });
+      return;
+    }
+    client.user?.setActivity(activity.activity, {
+      type: ActivityType[activity.type],
+      url: activity.url || undefined,
+    });
+
+    consola.success({
+      message: "Discord has been activity set",
+      badge: true,
+    });
+  } catch (err) {
+    consola.error({
+      message: `Error setting custom discord activity: ${err}`,
+      badge: true,
+    });
+  }
+};
