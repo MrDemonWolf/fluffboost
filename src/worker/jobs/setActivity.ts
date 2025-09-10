cf4ef40ce961deb66b1d6c2d10a3da9a65c3bd3e
Nathanial Henniges <nathanial.henniges@mrdemonwolf.com>
2025-09-10 00:12:01 -0500

    fix: Enhance Discord bot activity setting robustness

--- src/worker/jobs/setActivity.ts
+++ src/worker/jobs/setActivity.ts
@@ -12,6 +12,12 @@ export default async (client: Client) => {
     const defaultActivityType = env.DISCORD_DEFAULT_ACTIVITY_TYPE;
     const defaultActivityUrl = env.DEFAULT_ACTIVITY_URL;
 
+    if (!client.user) {
+      return logger.warn(
+        "Discord - Activity",
+        "Client user is not defined, cannot set activity"
+      );
+    }
     const randomActivity = async () => {
       const activities = await prisma.discordActivity.findMany();
 
@@ -39,22 +45,27 @@ export default async (client: Client) => {
         "Discord - Activity",
         "No custom discord activity found, using default activity"
       );
-      return client.user?.setActivity(defaultActivity, {
+      return client.user.setActivity(defaultActivity, {
         type: ActivityType[defaultActivityType],
         url: defaultActivityUrl,
       });
     }
 
-    client.user?.setActivity(activity.activity, {
+    client.user.setActivity(activity.activity, {
       type: ActivityType[activity.type],
-      url: activity.url || undefined,
+      url: activity.url ? activity.url : undefined,
     });
 
     logger.success("Discord - Activity", "Activity has been set", {
       activity: activity.activity,
       type: activity.type,
     });
+    return true;
   } catch (err) {
-    logger.error("Discord - Activity", "Error setting custom discord activity", err);
+    logger.error(
+      "Discord - Activity",
+      "Error setting custom discord activity",
+      err
+    );
   }
 };
